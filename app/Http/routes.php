48a79f90850a4eed1f0a2c058fc0109df7dd4d5c
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-02-27 23:05:21 +0700

    Add registration route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,4 +3,4 @@
 Route::get('/', 'WelcomeController@index');
 Route::get('home', ['as' => 'home', 'uses' => 'HomeController@index',]);
 
-
+Route::resource('registration', 'RegistrationController', ['only' => ['create', 'store']]);
